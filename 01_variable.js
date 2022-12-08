8bc8f01aebfd4feafb120c319ef187ca87960a75
shubhamzanwar333 <shubham.zanwar333@gmail.com>
2022-12-08 09:01:00 +0530

    variable.js
    JS started. First lecture on variable with its assignment.

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var myName; // variable declaration
+myName = "Shubham Zanwar"; // variable initialisation
+console.log(myName);
+
+var myFriend = "Nikhil Sabale";
+console.log(myFriend);
+
+myFriend = "abc";
+console.log(myFriend);
+
---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Shubham Zanwar";
+console.log(myFullName);
+
+myFullName = "Shubham Vitthaldas Zanwar";
+console.log(myFullName);
+
+var myLove ="I Love Only JS";
+console.log(myLove);
+
+var firstName = "Shubham";
+var lastName = "Zanwar";
+var age = "26";
+var address = "Pune";
+var pin_code = "411028";
+var gender = "Male";
+var is_married = "No";
+
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
